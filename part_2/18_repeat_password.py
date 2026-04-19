5cde3cc9ab713397e8bcc49d1993afe7ad6da593
Rahul Khadoliya <rahulkhadoliya1122@gmail.com>
2026-04-19 10:47:25 +0530

    Add 15th to 18th solved exercises

---
+++ part_2/18_repeat_password.py
@@ -0,0 +1,27 @@
+# Please write a program which asks the user for a password. The program should then ask the user to type in the password again. If the user types in something else than the first password, the program should keep on asking until the user types the first password again correctly.
+
+# Have a look at the expected behaviour below:
+
+# Sample output
+# Password: sekred
+# Repeat password: secret
+# They do not match!
+# Repeat password: cantremember
+# They do not match!
+# Repeat password: sekred
+# User account created!
+
+
+# Solution:
+
+password = input("Password: ")
+while True :
+    check_password = input("Repeat password: ")
+    if password != check_password:
+        print("They do not match!")
+    elif password == check_password:
+        print("User account created!")
+        break
+
+
+
