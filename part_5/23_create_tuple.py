25ac38ac71ac8b53887d32bb110a19e4011e5226
Rahul Khadoliya <rahulkhadoliya1122@gmail.com>
2026-08-26 15:09:16 +0530

    Added some other simple solutions

--- part_5/23_create_tuple.py
+++ part_5/23_create_tuple.py
@@ -14,24 +14,33 @@
 
 ## Solution:
 
+# def create_tuple(x : int, y : int, z : int):
+#     sum = x+y+z
+#     if x < y and x < z:
+#         if y>z:
+#             new_tuple = (x, y, sum)
+#         elif z>y:
+#             new_tuple = (x, z, sum)
+#     elif y < x and y < z:
+#         if x>z:
+#             new_tuple = (y, x, sum)
+#         elif z>x:
+#             new_tuple = (y, z, sum)
+#     if z < x and z < y:
+#         if y>x:
+#             new_tuple = (z, y, sum)
+#         elif x>y:
+#             new_tuple = (z, x, sum)
+#     return new_tuple
+
+# ## Simple use of sorted() function
+# def create_tuple(x : int, y : int, z : int):
+#     temp_list = sorted([x, y, z])
+#     return (temp_list[0], temp_list[-1], sum(temp_list))
+
+## Use of Min and Max functions
 def create_tuple(x : int, y : int, z : int):
-    sum = x+y+z
-    if x < y and x < z:
-        if y>z:
-            new_tuple = (x, y, sum)
-        elif z>y:
-            new_tuple = (x, z, sum)
-    elif y < x and y < z:
-        if x>z:
-            new_tuple = (y, x, sum)
-        elif z>x:
-            new_tuple = (y, z, sum)
-    if z < x and z < y:
-        if y>x:
-            new_tuple = (z, y, sum)
-        elif x>y:
-            new_tuple = (z, x, sum)
-    return new_tuple
+    return (min(x, y, z), max(x, y, z), x+y+z)
 
 if __name__ == "__main__":
     print(create_tuple(100, 102, 303))
